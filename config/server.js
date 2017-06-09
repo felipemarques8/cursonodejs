8c177fd3222649d07b1f26d410b758c4fe2b722b
Felipe Marques <lipemarques8@gmail.com>
2017-06-08 23:54:10 -0300

    Up module config

---
+++ config/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+module.exports = app;
